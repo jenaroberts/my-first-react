b12a78283661d552bfe01239d23753e2b73e5467
Jena Roberts <jenaroberts@Jenas-MacBook-Air.local>
2022-05-12 16:50:28 -0400

    added state into react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [firstName, setFirstName] = useState("*");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const logInLogOut = () => setIsLoggedIn(!isLoggedIn);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>You clicked the button {count} times.</h1>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount(0)}>Reset.</button>
+        <button onClick={() => setCount(Math.max(count - 1, 0))}>-</button>
+
+        <h2>Welcome Guest</h2>
+        <button onClick={logInLogOut}>
+          {isLoggedIn ? "Log Out" : "Log In"}{" "}
+        </button>
+
+        <section>
+          <h2 onClick={() => setFirstName(firstName + "*")}>
+            Hello {firstName}
+          </h2>
+        </section>
       </header>
     </div>
   );
